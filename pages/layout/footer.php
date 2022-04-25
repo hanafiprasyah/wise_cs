a5e051a4a8f957c625c450f32c5964ab4416bac6
hanafiprasyah <prasyah1998@gmail.com>
2022-04-25 09:39:33 +0700

    Membuat perubahan posisi pada footer-text

--- pages/layout/footer.php
+++ pages/layout/footer.php
@@ -1,13 +1,13 @@
 <?php
 $year = date("Y");
 ?>
-<footer class="main-footer">
+<footer class="main-footer text-center align-middle">
   <strong>Copyright &copy; 2017-<?php echo $year; ?> <a href="https://wise.co.id/">Wibawa Solusi Elektrik</a>.</strong>
   All rights reserved.
-  <div class="float-right d-none d-sm-inline-block">
+  <!-- <div class="float-right d-none d-sm-inline-block">
     <b>Beta Version</b> 0.3.8
-    <!-- #1 : desc the version of deploy -->
-    <!-- #2 : desc the frequency of developing the feature -->
-    <!-- #3 : desc the version of revision -->
-  </div>
+    #1 : desc the version of deploy
+    #2 : desc the frequency of developing the feature
+    #3 : desc the version of revision
+  </div> -->
 </footer>
